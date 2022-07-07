447e81993d2e987bf80bd36fe3bf99cc5e7ec50d
Evgenii Mykhalichenko <emykhailichenko@newage.io>
2022-07-07 20:32:17 +0200

    Init snippet

---
+++ tests/CsGOTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SteamMarketProviders\CsGO\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CsGOTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        $this->assertTrue(true);
+    }
+}
