cee667109f484dca58228462d4c99e82c4a11057
Evgenii Mykhalichenko <emykhailichenko@newage.io>
2022-07-09 21:08:05 +0200

    Update project

--- src/CsGOProvider.php
+++ src/CsGOProvider.php
@@ -4,6 +4,31 @@ declare(strict_types=1);
 
 namespace SteamMarketProviders\CsGO;
 
-class CsGOProvider
+use SteamMarketProviders\Enums\SteamApp;
+use SteamMarketProviders\Enums\SteamLanguage;
+use SteamMarketProviders\ParserManager\Builder\ParseRulesBuilder;
+use SteamMarketProviders\ParserManager\Builder\SearchUrlBuilder;
+use SteamMarketProviders\ParserManager\Contract\UrlBuilderInterface;
+use SteamMarketProviders\ParserManager\Parser\Provider\AbstractProvider;
+
+class CsGOProvider extends AbstractProvider
 {
+    protected function createUrl(int $page): UrlBuilderInterface
+    {
+        return (new SearchUrlBuilder())
+            ->setAppId(SteamApp::CSGO)
+            ->setPage($page)
+            ->setLanguage(SteamLanguage::Russian);
+    }
+
+    protected function createParseRules(): ParseRulesBuilder
+    {
+        return (new ParseRulesBuilder())
+            ->wrapper('wrapper1', '.div', function(ParseRulesBuilder $builder) {
+                $builder->item('image', '.sdas')
+                    ->wrapper('wrapper2', '.lox', function(ParseRulesBuilder $builder) {
+                        $builder->item('image2', '.sdas');
+                    });
+            });
+    }
 }
